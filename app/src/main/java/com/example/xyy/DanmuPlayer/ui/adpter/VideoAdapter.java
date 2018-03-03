08175f751ade9dbccc85498b9fd4a65fc9ebc09b
xyy <yeshao1997@outlook.com>
2018-03-03 18:20:46 +0800

    修复视频缩略图

--- app/src/main/java/com/example/xyy/DanmuPlayer/ui/adpter/VideoAdapter.java
+++ app/src/main/java/com/example/xyy/DanmuPlayer/ui/adpter/VideoAdapter.java
@@ -1,42 +1,30 @@
 package com.example.xyy.DanmuPlayer.ui.adpter;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.media.MediaMetadataRetriever;
-import android.os.Environment;
-import android.os.Handler;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.xyy.DanmuPlayer.bean.Video;
 import com.example.xyy.DanmuPlayer.R;
-import com.example.xyy.DanmuPlayer.ui.activities.MainActivity;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.assist.FailReason;
-import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+import com.example.xyy.DanmuPlayer.bean.Video;
+import com.example.xyy.DanmuPlayer.utils.ImageTask;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.util.List;
 
-import io.vov.vitamio.ThumbnailUtils;
-import io.vov.vitamio.provider.MediaStore;
 import io.vov.vitamio.utils.StringUtils;
 
 public class VideoAdapter extends BaseAdapter{
-    Context context;
-    List<Video> data;
-    Handler handler;
+
+    private Context context;
+    private List<Video> data;
+    private ImageTask it;
 
     public VideoAdapter(Context context, List<Video> data){
         this.context = context;
         this.data = data;
-        handler = new Handler();
     }
 
     @Override
@@ -56,6 +44,9 @@ public class VideoAdapter extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
+        it = new ImageTask(parent);
+
         View v;
         if (convertView != null)
             v = convertView;
@@ -64,7 +55,6 @@ public class VideoAdapter extends BaseAdapter{
 
         Video video = data.get(position);
         String video_name_text = video.getvideo_name();
-        //String video_path = "file://"+video.getvideo_path();
         String video_path = video.getvideo_path();
         int video_time_text = video.getvideo_time();
 
@@ -73,30 +63,10 @@ public class VideoAdapter extends BaseAdapter{
         TextView video_time = (TextView)v.findViewById(R.id.video_time);
         RelativeLayout time_rl = (RelativeLayout)v.findViewById(R.id.time_rl);
 
-        ImageLoader imageLoader = ImageLoader.getInstance();
-        imageLoader.displayImage(video_path , video_iv, new ImageLoadingListener() {
-            @Override
-            public void onLoadingStarted(String s, View view) {
-
-            }
-
-            @Override
-            public void onLoadingFailed(String s, View view, FailReason failReason) {
-
-            }
-
-            @Override
-            public void onLoadingComplete(String s, View view, Bitmap bitmap) {
-
-            }
-
-            @Override
-            public void onLoadingCancelled(String s, View view) {
-
-            }
-        });
-
         video_name.setText(video_name_text);
+        video_iv.setTag(video_path);
+        it.execute(video_path);
+
         if (video_time_text == 0){
             time_rl.setVisibility(View.GONE);
         }else {
@@ -104,5 +74,5 @@ public class VideoAdapter extends BaseAdapter{
         }
         return v;
     }
-    
+
 }
